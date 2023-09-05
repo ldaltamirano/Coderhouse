60295cadbc482d84c3fce1ca9fe9337790ea1278
ldaltamirano <lalt123@gmail.com>
2023-09-05 19:20:51 -0300

    Se agrego badge al cartWidget

--- React/PreEntrega1-Altamirano/src/Components/CartWidget/CartWidget.jsx
+++ React/PreEntrega1-Altamirano/src/Components/CartWidget/CartWidget.jsx
@@ -1,5 +1,5 @@
 import "./CartWidget.css"
 
 export default function CartWidget() {
-    return <i className="fa-solid fa-cart-shopping"><span className="translate-middle badge rounded-pill bg-dange">1</span></i>
+    return <i className="fa-solid fa-cart-shopping"><span className="top-0 start-100 translate-middle badge rounded-pill bg-danger">1</span></i>
 }
